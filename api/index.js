8d13d78203ffd46ac847e5f16d77dffa8c14b609
panda6151025 <liamhu0914@gmail.com>
2024-10-21 10:05:36 -0300

    Import module

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const { LinkedInProfileScraper } = require("linkedin-profile-scraper");
 const app = express();
 const PORT = 5000;
 
