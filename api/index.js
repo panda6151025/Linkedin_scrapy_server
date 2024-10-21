853e52789ac1d22b5f9651fcd3e395a3497e189b
panda6151025 <liamhu0914@gmail.com>
2024-10-21 09:42:13 -0300

    Update Server

--- api/index.js
+++ api/index.js
@@ -11,6 +11,19 @@ app.use(cors());
 app.get("/", async (req, res) => {
   return res.json({ flag: "true", message: "server is working" });
 });
+app.post("/getInfo", async (req, res) => {
+  const { url, lib_at } = req.body;
+  const scraper = new LinkedInProfileScraper({
+    sessionCookieValue: lib_at,
+    keepAlive: false,
+  });
+
+  await scraper.setup();
+
+  const result = await scraper.run(url);
+
+  console.log(result);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
